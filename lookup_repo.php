f02826930eaaf9ce9a515c1b5ae8ade06a9d7dc5
Dirk Thomas <dthomas@willowgarage.com>
2013-01-25 14:58:21 -0800

    remove obsolete files

--- lookup_repo.php
+++
@@ -1,7 +0,0 @@
-<?php
-$xml = simplexml_load_file("megamanifest.xml");
-$xquery = "/pkgs/package[@name='{$_GET['pkg']}']";
-$pkg = $xml->xpath($xquery);
-$pkg = $pkg[0];
-echo $pkg['repo_host'];
-?>
