b484c674c4c46592a6a9f92eb6568fc4956878f8
Tully Foote <tfoote@willowgarage.com>
2013-02-15 13:36:54 -0800

    typo in distro variable fixes #1

--- search.php
+++ search.php
@@ -99,7 +99,7 @@ foreach($stack_results as $name => $stack)
   $maintainers = format_names($pkg['maintainers']);
   $authors = format_names($pkg['authors']);
   $maintainers_and_authors = $maintainers + $authors;
-  echo '<tr><td class="pkgname"><a href="details.php?distro=' . urlencode($distroi) . '&name=' . urlencode($name) . '">' . $name . '</a></td><td>' . implode(', ', $maintainers_and_authors) . '</td><td>' . htmlentities($desc) . '</td></tr>';
+  echo '<tr><td class="pkgname"><a href="details.php?distro=' . urlencode($distro) . '&name=' . urlencode($name) . '">' . $name . '</a></td><td>' . implode(', ', $maintainers_and_authors) . '</td><td>' . htmlentities($desc) . '</td></tr>';
   echo "\n";
 }
 ?>
@@ -129,7 +129,7 @@ foreach($package_results as $name => $pkg)
   $maintainers = format_names($pkg['maintainers']);
   $authors = format_names($pkg['authors']);
   $maintainers_and_authors = $maintainers + $authors;
-  echo '<tr><td class="pkgname"><a href="details.php?distro=' . urlencode($distroi) . '&name=' . urlencode($name) . '">' . $name . '</a></td><td>' . implode(', ', $maintainers_and_authors) . '</td><td>' . htmlentities($desc) . '</td></tr>';
+  echo '<tr><td class="pkgname"><a href="details.php?distro=' . urlencode($distro) . '&name=' . urlencode($name) . '">' . $name . '</a></td><td>' . implode(', ', $maintainers_and_authors) . '</td><td>' . htmlentities($desc) . '</td></tr>';
   echo "\n";
 }
 ?>
