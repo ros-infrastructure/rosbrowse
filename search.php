5652b473adeb66293f8edb67511a5c3405abc487
William Woodall <william@osrfoundation.org>
2015-05-22 16:48:14 -0500

    updates for jade release

--- search.php
+++ search.php
@@ -2,8 +2,8 @@
 $title = "search results";
 include "rosbrowse.inc";
 
-$distro = 'indigo';
-$distro_list = array("fuerte", "groovy", "hydro", "indigo", "jade");
+$distro = 'jade';
+$distro_list = array("fuerte", "groovy", "hydro", "indigo", "jade", "kame");
 if (isset($_GET['distro']) && !empty($_GET['distro']))
 {
   $distro = $_GET['distro'];
